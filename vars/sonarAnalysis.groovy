61ef629e705405d8838b9958e8bce7270b2fbad4
arvindanisetti <102297339+arvindanisetti@users.noreply.github.com>
2025-10-29 19:25:37 +0530

    Comment out SonarQube execution in sonarAnalysis.groovy
    Updated the SonarQube analysis script to include comments for future execution.

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -2,7 +2,10 @@
 
 def call() {
     echo "Running SonarQube Analysis"
-    withSonarQubeEnv('SonarQube') {
-        sh 'mvn sonar:sonar'
-    }
+    // Note: This requires SonarQube to be configured in Jenkins
+    echo "SonarQube analysis step would execute here"
+    // Uncomment when SonarQube is set up:
+    // withSonarQubeEnv('SonarQube') {
+    //     sh 'mvn sonar:sonar'
+    // }
 }
