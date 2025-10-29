230f538f395eb3917b75df069a1e59ecf7cada27
arvindanisetti <102297339+arvindanisetti@users.noreply.github.com>
2025-10-29 19:50:37 +0530

    Change default Maven goals from 'clean install' to 'clean clean'

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String mavenGoals = 'clean install') {
+def call(String mavenGoals = 'clean clean') {
     echo "Building with Maven: ${mavenGoals}"
     sh "mvn ${mavenGoals}"
 }
