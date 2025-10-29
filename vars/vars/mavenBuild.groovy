b2993e7beedbf601dd4e3d2c5d640874ca5aa27a
arvindanisetti <102297339+arvindanisetti@users.noreply.github.com>
2025-10-29 19:22:08 +0530

    Enhance Maven build message with goals

--- vars/vars/mavenBuild.groovy
+++ vars/vars/mavenBuild.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String mavenGoals = 'clean install') {
-    echo "Building with Maven..."
+    echo "Building with Maven: ${mavenGoals}"
     sh "mvn ${mavenGoals}"
 }
